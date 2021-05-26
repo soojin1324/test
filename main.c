449bce3c74b022f3abd50e66935eae679079cdf2
soojin0215 <soojinkim0215@gmail.com>
2021-05-26 19:27:27 +0900

    이름 입력 및 출력

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main()
 {
+	char name[10];
 	int age;
 	
-	printf("당신의 나이를 입력하세요 : ");
+	printf("당신의 이름을 입력하세요 : ");
+	scanf("%s", name);
 	
+	printf("당신의 나이를 입력하세요 : ");
 	scanf("%d", &age);
 	
-	printf("%d살 이시군요!", age);
+	printf("반갑습니다 %s님!\n당신은 %d살 이시군요!", name,age);
 }
